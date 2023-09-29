98b84e961ea1bd1f2b62d5692b058b4573c7981b
MikeUnife <115147832+MikeUnife@users.noreply.github.com>
2023-09-29 11:48:08 +0200

    Add files via upload

---
+++ CalcNum01.m
@@ -0,0 +1,13 @@
+% CalcNum01 - Primo Esercizio
+% Descrizione
+% ver. 1.0
+ver;
+% keyboard;
+
+x=10
+
+% convertito=input('inserisci un numero');
+function [ convertito ] =  interoDaBase10aBaseB(x);
+fprintf(convertito);
+
+end
